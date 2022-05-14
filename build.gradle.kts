959c2eb1345d8fd0aa085c9b5f5cbf80edf4d4af
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-05-14 02:39:32 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.7"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+	jacoco
+}
+
+group = "com.cybermakers"
+version = "0.0.1"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+jacoco {
+	toolVersion = "0.8.5"
+}
+
+val excludePackages: Iterable<String> = listOf(
+	"**/com/cybermakers/gitrankingcommits/application/config/**",
+	"**/com/cybermakers/gitrankingcommits/domain/entities/**"
+)
+
+extra["excludePackages"] = excludePackages
+
+tasks.jacocoTestReport {
+	reports {
+		xml.isEnabled = true
+		html.isEnabled = true
+		html.destination = file("$buildDir/reports/jacoco")
+	}
+
+	classDirectories.setFrom(
+		sourceSets.main.get().output.asFileTree.matching {
+			exclude(excludePackages)
+		}
+	)
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				minimum = 0.6.toBigDecimal()
+			}
+		}
+	}
+
+	classDirectories.setFrom(
+		sourceSets.main.get().output.asFileTree.matching {
+			exclude(excludePackages)
+		}
+	)
+
+	mustRunAfter(tasks["jacocoTestReport"])
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.2"
+
+dependencies {
+	// Observability
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	// Persistance
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("redis.clients:jedis")
+	implementation("io.lettuce:lettuce-core")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+	//Web
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// Logger
+	implementation("org.slf4j:slf4j-api:1.7.36")
+
+	// Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	// Dev
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.ninja-squad:springmockk:3.1.1")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+	testImplementation("it.ozimov:embedded-redis:0.7.3")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/cybermakers/gitrankingcommits/GitRankingCommitsApplication.kt
@@ -0,0 +1,11 @@
+package com.cybermakers.gitrankingcommits
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitRankingCommitsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GitRankingCommitsApplication>(*args)
+}
